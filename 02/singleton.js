0b374c102dea35d91082a9c8ade0f18619f9bdcd
Nick <mantonik@touchcommerce.com>
2019-06-11 13:25:38 +0300

    update singleton to pass test

--- 02/singleton.js
+++ 02/singleton.js
@@ -2,6 +2,11 @@
 
 var Singleton = new function(){
     var instance = this;
-    return function(){return instance}
+    return function inner(){
+		instance.__proto__ = inner.prototype;
+		return instance}
 }
 console.log(new Singleton === new Singleton);
+console.log(typeof new Singleton() === "object");
+console.log(new Singleton() instanceof Singleton === true);
+console.log(new Singleton() === new Singleton());
